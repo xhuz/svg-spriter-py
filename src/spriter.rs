427f6ef515b0a7d0c352eaaedf7ad1f0c24a05eb
chenzhiyuan <chenzhiyuan@wisdomgarden.com>
2025-10-21 17:08:21 +0800

    fix: fix svg sprite[LMS-13666]

--- src/spriter.rs
+++ src/spriter.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, fs, path::Path};
 use xmltree::{Element, EmitterConfig, Namespace, XMLNode};
 
 const EXCLUDE_ATTRS: [&str; 2] = ["fill", "stroke"];
+const WHITE_COLORS: [&str; 3] = ["white","#ffffff","#fff"];
 
 #[derive(Debug)]
 pub struct Svg {
@@ -68,7 +69,7 @@ where
                 let mut new_ele = Element::new(element_name);
                 let attrs = &element.attributes;
                 attrs.iter().for_each(|(k, v)| {
-                    if !EXCLUDE_ATTRS.contains(&k.as_str()) {
+                    if !EXCLUDE_ATTRS.contains(&k.as_str()) || !WHITE_COLORS.contains(&v.as_str()) {
                         new_ele.attributes.insert(k.to_string(), v.to_string());
                     }
                 });
